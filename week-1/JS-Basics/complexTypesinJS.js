0f47c3570cae622c87cef23b5c3ee40609c3ef4b
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-08-04 23:03:51 +0530

    commit

---
+++ week-1/JS-Basics/complexTypesinJS.js
@@ -0,0 +1,61 @@
+// 1. Write a function that takes a user as an input and greets them with their name and age
+
+function greetFun() {
+  return "Hello!" + " " + user.name + " " + "age" + " " + user.age;
+}
+
+let user = {
+  name: "vaish",
+  age: 22,
+};
+console.log(greetFun(user));
+
+// 2. Write a function that takes a new object as input which has name , age  and gender and greets the user with their gender (Hi Mr/Mrs/Others harkirat, your age is 21)
+// 3. Also tell the user if they are legal to vote or not
+
+function greetFun1(user2) {
+  if (user2.gender == "female") {
+    pre_name = "Mrs";
+  } else if (user2.gender == "male") {
+    pre_name = "Mr";
+  } else {
+    pre_name = "Others";
+  }
+  let res =
+    "Hi" +
+    " " +
+    pre_name +
+    " " +
+    user2.name +
+    ", " +
+    "your age is" +
+    " " +
+    user2.age +
+    ". ";
+  let res1;
+  if (user2.age > 18) {
+    res1 = "You can vote.";
+  } else {
+    res1 = "You can't vote.";
+  }
+  return res + res1;
+}
+
+let user2 = {
+  name: "vaishnavi",
+  age: 22,
+  gender: "female",
+};
+console.log(greetFun1(user2));
+
+// Arrays - 1. Write a function that takes an array of numbers as input, and returns a new array with only even values. Read about filter in JS
+
+function newArr(arr1) {
+  return arr1.filter();
+}
+let arr1 = [2, 3, 4, 5, 6, 7, 8, 9];
+console.log(newArr(arr1));
+
+// 2. Write a function that takes an array of users as inputs and returns only the users who are more than 18 years old
+
+// 3. create a function that takes an array of objects as input, and returns the users whose age>18 and are male
