8b406951dda66345eb2ef0146971aeb4913a6105
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-08-05 18:53:25 +0530

    finished the assignments

--- week-1/JS-Basics/complexTypesinJS.js
+++ week-1/JS-Basics/complexTypesinJS.js
@@ -51,11 +51,34 @@ console.log(greetFun1(user2));
 // Arrays - 1. Write a function that takes an array of numbers as input, and returns a new array with only even values. Read about filter in JS
 
 function newArr(arr1) {
-  return arr1.filter();
+  return arr.filter((num) => num % 2 === 0);
 }
 let arr1 = [2, 3, 4, 5, 6, 7, 8, 9];
 console.log(newArr(arr1));
 
 // 2. Write a function that takes an array of users as inputs and returns only the users who are more than 18 years old
 
+function filterAdults(users) {
+  return users.filter((user) => user.age > 18);
+}
+
+let users = [
+  { name: "vaish1", age: 17 },
+  { name: "vaish2", age: 22 },
+  { name: "vaish3", age: 16 },
+  { name: "vaish4", age: 19 },
+];
+console.log(filterAdults(users));
+
 // 3. create a function that takes an array of objects as input, and returns the users whose age>18 and are male
+function filterAdults(users) {
+  return users.filter((user) => user.age > 18);
+}
+
+let users1 = [
+  { name: "vaish1", age: 17, gender: "male" },
+  { name: "vaish2", age: 22, gender: "female" },
+  { name: "vaish3", age: 16, gender: "female" },
+  { name: "vaish4", age: 19, gender: "male" },
+];
+console.log(filterAdults(users));
