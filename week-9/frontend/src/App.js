ac5d25d641e04420b74dd8a4558f99c07b492aa6
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-10-05 23:48:26 +0530

    wrote notes for a problem

--- week-9/frontend/src/App.js
+++ week-9/frontend/src/App.js
@@ -5,10 +5,13 @@ function App() {
   const [count, setCount] = useState(0);
 
   function onClickButton() {
-    
-    setCount(count+1);
+    setCount(count + 1);
   }
   return <button onClick={onClickButton}>counter {count}</button>;
 }
 
 export default App;
+
+// NOTES
+// count++ tries to modify count, which is immutable.
+//  count + 1 computes the new value based on the current value of count
