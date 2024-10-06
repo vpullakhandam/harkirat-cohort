9abf4e248f960a39e7da4189b54dda94762a0249
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-10-06 21:40:52 +0530

    commented the previous code

--- week-9/frontend/src/App.js
+++ week-9/frontend/src/App.js
@@ -1,17 +1,21 @@
-import React from "react";
-import { useState } from "react";
+// import React from "react";
+// import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+// function App() {
+//   const [count, setCount] = useState(0);
 
-  function onClickButton() {
-    setCount(count + 1);
-  }
-  return <button onClick={onClickButton}>counter {count}</button>;
-}
+//   function onClickButton() {
+//     setCount(count + 1);
+//   }
+//   return <button onClick={onClickButton}>counter {count}</button>;
+// }
 
-export default App;
+// export default App;
 
 // NOTES
 // count++ tries to modify count, which is immutable.
 //  count + 1 computes the new value based on the current value of count
+
+
+// Day 2 week-9!!
+
