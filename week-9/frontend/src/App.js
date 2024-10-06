d78b3a4a31d5c5ca9d4876b7030c9a90b347e582
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-10-06 21:44:55 +0530

    updated counters

--- week-9/frontend/src/App.js
+++ week-9/frontend/src/App.js
@@ -16,6 +16,27 @@
 // count++ tries to modify count, which is immutable.
 //  count + 1 computes the new value based on the current value of count
 
-
 // Day 2 week-9!!
 
+import React from "react";
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function incrementCounter() {
+    setCount(count + 1);
+  }
+  function decrementCounter() {
+    setCount(count - 1);
+  }
+  function resetCounter() {
+    setCount(count);
+  }
+
+  return <button onClick={incrementCounter}>Increment Counter {count}</button>;
+  return <button onClick={decrementCounter}>Decrement Counter</button>;
+  return <button onClick={resetCounter}>Reset Counter</button>;
+}
+
+export default App;
