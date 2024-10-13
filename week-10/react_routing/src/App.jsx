dbac3f8ce5b2031b510bada5988ccd032c3edc0f
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-10-13 19:42:26 +0530

    added routing

--- week-10/react_routing/src/App.jsx
+++ week-10/react_routing/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Class11Program />} />
+          <Route
+            path="/neet/online-coaching-class-11"
+            element={<Class11Program />}
+          />
+          <Route
+            path="/neet/online-coaching-class-12"
+            element={<Class12Program />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 
-export default App
+  function Class11Program() {
+    return <div>NEET Programs for Class 11th</div>;
+  }
+  function Class12Program() {
+    return <div>NEET Programs for Class 12th</div>;
+  }
+}
+export default App;
